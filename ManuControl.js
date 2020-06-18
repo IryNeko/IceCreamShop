f28efbb35fb170f42ab786de8e226fece7aa4f93
IryNeko <31180530+IryNeko@users.noreply.github.com>
2020-06-18 00:27:48 -0400

    Add files via upload

--- ManuControl.js
+++ ManuControl.js
@@ -1,7 +1,7 @@
 //function constructor:
 //Item:name，Price,Calores,picture,description,
 function showItem(name,price,cal,pic,des){
-	alert("debug:"+name);
+	//alert("debug:"+name);
 	document.getElementById("manu_des").innerHTML="Item Name:"+name+"<br>"+"Price:"+price+"<br>Calories:"+cal+"<br>"+des;
 	document.getElementById("manu_img").src=pic;
 }
