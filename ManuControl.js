cd0e58a6e1e0af981b1c70735802faa034f8917f
IryNeko <31180530+IryNeko@users.noreply.github.com>
2020-06-19 14:49:53 -0400

    Add files via upload

--- ManuControl.js
+++ ManuControl.js
@@ -4,4 +4,14 @@ function showItem(name,price,cal,pic,des){
 	//alert("debug:"+name);
 	document.getElementById("manu_des").innerHTML="Item Name:"+name+"<br>"+"Price:"+price+"<br>Calories:"+cal+"<br>"+des;
 	document.getElementById("manu_img").src=pic;
+	itemPos();
+}
+function itemPos(){
+	//change the item position based on the image width
+	//the element width is 700, so should leave that much for image
+	//var width=document.getElementById("manu_img").width;
+	//alert(width);
+	//var add=(700-parseInt(width))/2;
+	//alert(add);
+	//document.getElementById("manu_img").marginLeft=add;
 }
