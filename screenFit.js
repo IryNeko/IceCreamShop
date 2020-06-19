bcb661c547c3c39330bc99514c2d20761bc005b9
IryNeko <31180530+IryNeko@users.noreply.github.com>
2020-06-19 15:59:46 -0400

    Add files via upload

--- screenFit.js
+++ screenFit.js
@@ -3,11 +3,15 @@
 function lookHere(){
 	if (screen.width < screen.height){
 	//alert(screen.width);
+	//var items=document.getElementsByClassName("board");
+	//for (var i=0;i<item.length;i++){
+	//	items[i].style.width="100%";
+	//}
+	alert("true");
 	var items=document.getElementsByClassName("board");
-	for (var i=0;i<item.length;i++){
-		items[i].style.width="100%";
-	}
-	
-	
+	var i=0;
+	for (i=0;i<items.length;i++){
+	items[i].style.width="100%";
+		}
 	}
 }
