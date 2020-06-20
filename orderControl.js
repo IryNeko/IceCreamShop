8d682f787483056f8ddf47402651d229daa9e9d0
IryNeko <31180530+IryNeko@users.noreply.github.com>
2020-06-20 17:13:29 -0400

    Add files via upload

---
+++ orderControl.js
@@ -0,0 +1,16 @@
+//first, get the cookie
+var x=document.cookie;
+//second, cut the cookie up
+//third, parse it to a good list
+function parseCookie(){
+	var cutCookies=document.cookie.split(';');
+	var cartList="";
+	for (var i=0;i<cutCookies.length;i++){
+		cartList+=cutCookies+"<br>";
+	}
+	//display the list on the innerHTML
+	document.getElementById("orders").innerHTML=cartList;
+	
+}
+	
+
