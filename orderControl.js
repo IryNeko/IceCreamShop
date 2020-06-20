c7c090e6b2b99368047c3aab8b3a9b93636c33ce
IryNeko <31180530+IryNeko@users.noreply.github.com>
2020-06-20 17:46:45 -0400

    Add files via upload

--- orderControl.js
+++ orderControl.js
@@ -3,19 +3,15 @@ var x=document.cookie;
 //second, cut the cookie up
 //third, parse it to a good list
 function parseCookie(){
-	//var cutCookies=document.cookie.split(';');
-	//var cartList="";
-	//for (var i=0;i<cutCookies.length;i++){
-	//	cartList+=cutCookies[i]+"<br>";
-	//}
-	//display the list on the innerHTML
-	//document.getElementById("orders").innerHTML=cartList;
-	var cutCookies=document.cookie.getAll();
+	var cutCookies=document.cookie.split(';');
 	var cartList="";
 	for (var i=0;i<cutCookies.length;i++){
 		cartList+=cutCookies[i]+"<br>";
-	}
+	//}
+	//display the list on the innerHTML
+	cartlist=cartList.replace("="," : ")
 	document.getElementById("orders").innerHTML=cartList;
+	
 }
 	
 
